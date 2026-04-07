70469bcabd54a82bee33979b3314af13180c079f
MasterMIS <mastermis031@gmail.com>
2026-04-07 09:26:46 +0530

    fix: added AUTH_SECRET and wrapped middleware for AWS Amplify

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import { authConfig } from "./auth.config";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   trustHost: true,
+  secret: process.env.AUTH_SECRET,
   providers: [
     Credentials({
       name: "Credentials",
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export default NextAuth(authConfig).auth;
+export default NextAuth(authConfig).auth((req) => {
+  // This helps NextAuth trust the Amplify production URL
+});
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
